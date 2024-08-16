cab3ad98a8932ccdf93f214701d76da9166a364b
Klisman Gabriel Bourscheidt <klisgabriel@gmail.com>
2024-08-15 22:46:30 -0300

    coup

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var copo = window.document.getElementById("copo")
 function esvaziarCopo(){
     copo.src = "copo_vazio.svg"
 }
+
+var copoVazio = window.document.getElementById("copo")
+
+function encherCopo(){
+    copoVazio.src = "copo_cheio.svg"
+}
